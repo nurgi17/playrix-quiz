4fbf2a76eb73a1a5b06944a5f1202a7be500fcbb
nurgi17 <usen.nurgisa@gmail.com>
2024-10-20 12:43:37 +0500

    Updates

---
+++ assets/game.service-CDa2DnL5.js
@@ -0,0 +1,1 @@
+import{u as o}from"./PlayrixLogo-BhTuJenN.js";import{h as r}from"./index-8e9M5aAb.js";function s(){const e=o(),t=r(()=>e.newGame);function m(){e.setNewGame()}return{setNewGame:m,getGame:t}}export{s as u};
